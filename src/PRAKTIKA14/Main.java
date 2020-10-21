bdc8ed20337f84b8b478ee9a013a2eb079746f75
gigagibi <gii01@mail.ru>
2020-10-22 01:58:15 +0300

    1

--- src/PRAKTIKA14/Main.java
+++ src/PRAKTIKA14/Main.java
@@ -3,16 +3,62 @@ package PRAKTIKA14;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static String replaceByRulesREGEX(ArrayList<String> rules, String str)
+    public static String replaceByRulesREGEX(ArrayList<String> rules, String str) //почему то m.group() возвращает только одно совпадение (не AA и 111, а только AA) хотя groupCount() = 2
     {
-        String line = str;
-        line.replaceAll("\\w+", "");
-        return str;
+        /*String regex = new String();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < rules.size(); i++)
+        {
+            sb.append(rules.get(i).split(" ")[0]);
+        }*/
+        Pattern p = Pattern.compile("AA|11");//sb.toString());
+        Matcher m = p.matcher("AA11AA1");
+        //return m.replaceAll("1");
+        //m.matches();
+        //return m.group(0).replaceAll("\\w+", "1");
+        StringBuilder bbb = new StringBuilder();
+
+        String b1 = new String();
+
+        int sc = 0;
+        while (m.find())
+        {
+            sc++;
+            //m.matches();
+            System.out.println(m.group());
+            b1 = m.replaceFirst("3");
+            //m = p.matcher(b1);
+            m.find(sc);
+        }
+       // b1 =
+        return b1;
     }
+
+    /*public static String replaceByRulesREGEX(ArrayList<String> rules, String str)
+    {
+
+        ArrayList<Pattern> patterns = new ArrayList<Pattern>();
+        ArrayList<Matcher> matchers = new ArrayList<Matcher>();
+        for(int i = 0; i < rules.size(); i++)
+        {
+            patterns.add(Pattern.compile(rules.get(i).split(" ")[i]));
+            matchers.add(patterns.get(i).matcher());
+        }
+        Pattern pattern = Pattern.compile("af");
+        Matcher matcher = pattern.matcher("afwdv3r thyth");
+        if(matcher.find())
+        {
+            return matcher.replaceAll("");
+        }
+        //matchers.get(i).
+        else
+            return "Error";
+    }*/
 
     public static String replaceByRules(ArrayList<String> rules, String str)
     {
@@ -29,16 +75,22 @@ public class Main {
         }
         int size = line.length();
 
-        while(e != size-1 && b != size-1)
+        while(e < size-1 && b < size-1)
         {
             StringBuilder sb = new StringBuilder();
-            while(replaceMap.get(sb.toString()) == null)
+            while(replaceMap.get(sb.toString()) == null && e < line.length())
             {
                 sb.append(line.charAt(e));
                 e++;
             }
-            b = e;
-            replaced.add(replaceMap.get(sb.toString()));
+            if(e < line.length())
+                b = e;
+            else {
+                replaced.add(String.valueOf(line.charAt(b)));
+                e = ++b;
+            }
+            if(replaceMap.get(sb.toString()) != null)
+                replaced.add(replaceMap.get(sb.toString()));
         }
         StringBuilder sb = new StringBuilder();
         for(String t : replaced)
@@ -70,5 +122,6 @@ public class Main {
 
         str = sc.nextLine();
         System.out.println(replaceByRules(rules, str));
+        //System.out.println(replaceByRulesREGEX(null, null));
     }
 }
